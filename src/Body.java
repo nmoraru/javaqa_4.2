dea0205cc53d3335e9d9b6354c1545c6e93d800f
nikita moraru <test@mail.com>
2020-04-05 19:40:45 +0300

    ref v.1

--- src/Body.java
+++ src/Body.java
@@ -1,15 +1,21 @@
 public class Body {
-    public static void calculateMassIndex(double weight, double height) {
+    public String calculateMassIndex(double weight, double height) {
         double ind = (weight * 10_000) / (height * height);
+        String comment;
         if (ind < 18.5) {
-            System.out.println("Недовес: ваш ИМТ " + ind + " меньше чем 18.5");
+            comment = "Недовес: ваш ИМТ " + ind + " меньше чем 18.5";
+            return comment;
         } else if (ind >= 18.5 && ind < 25) {
-            System.out.println("Нормальный вес: ваш ИМТ " + ind + " находится между 18.5 и 25");
+            comment = "Нормальный вес: ваш ИМТ " + ind + " находится между 18.5 и 25";
+            return comment;
         } else if (ind >= 25 && ind < 30) {
-            System.out.println("Избыточный вес: ваш ИМТ " + ind + " находится между 25 и 30");
+            comment = "Избыточный вес: ваш ИМТ " + ind + " находится между 25 и 30";
+            return comment;
         } else if (ind >= 30) {
-            System.out.println("Ожирение: ваш ИМТ " + ind + " больше или равен 30");
+            comment = "Ожирение: ваш ИМТ " + ind + " больше или равен 30";
+            return comment;
         } else {
+            return "";
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,15 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        Body IMT = new Body();
         BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите ваш вес в кг");
         double weight = Double.parseDouble(bis.readLine()); // вес
         System.out.println("Введите ваш рост в см");
         double height = Double.parseDouble(bis.readLine()); // рост
 
-        Body.calculateMassIndex(weight, height);
+        String comment = IMT.calculateMassIndex(weight, height);
+        System.out.println(comment);
     }
 }
 
